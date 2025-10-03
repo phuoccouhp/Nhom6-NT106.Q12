92986432d99fc584155f8930f3c90aa65245daec
phuoccouhp <huyphuoc09112005@gmail.com>
2025-10-03 21:52:32 +0700

    Khởi tạo form
    phướcc khởi tạo form

---
+++ BTVN1-Nhom6-NT106.Q12/Form1.cs
@@ -0,0 +1,10 @@
+namespace BTVN1_Nhom6_NT106.Q12
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ BTVN1-Nhom6-NT106.Q12/Program.cs
@@ -0,0 +1,17 @@
+namespace BTVN1_Nhom6_NT106.Q12
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
+        }
+    }
+}
