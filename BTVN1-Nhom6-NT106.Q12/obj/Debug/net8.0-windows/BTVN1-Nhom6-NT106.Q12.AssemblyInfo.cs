b3b454a92b98e8242fd93c730dff71e34fd8597d
phuoccouhp <huyphuoc09112005@gmail.com>
2025-10-07 01:45:56 +0700

    hiệu chỉnh
    hiệu chỉnh

--- BTVN1-Nhom6-NT106.Q12/obj/Debug/net8.0-windows/BTVN1-Nhom6-NT106.Q12.AssemblyInfo.cs
+++ BTVN1-Nhom6-NT106.Q12/obj/Debug/net8.0-windows/BTVN1-Nhom6-NT106.Q12.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BTVN1-Nhom6-NT106.Q12")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f85270e86453dbd8742ac67c6564c8a66c05dfbd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb0f3e6aed8bba2fac47bd284e0834b68773f423")]
 [assembly: System.Reflection.AssemblyProductAttribute("BTVN1-Nhom6-NT106.Q12")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BTVN1-Nhom6-NT106.Q12")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
